7833e261153d3e5cbca6f938c407ff0907d9072b
shurbway <yabdust@gmail.com>
2024-05-19 19:35:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Car rental showcase Project
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["cdn.imagin.studio", "picsum.photos"]
+    }
+}
+
+module.exports = nextConfig
